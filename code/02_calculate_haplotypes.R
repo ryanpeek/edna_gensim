a6b3c63c578e18947fafc0041d0b09855e57e442
Ryan Peek <rapeek@ucdavis.edu>
2020-08-28 09:29:11 -0700

    update code to do by locus or rep

---
+++ code/02_calculate_haplotypes.R
@@ -0,0 +1,147 @@
+# Make haplotype plots
+
+# Libraries ---------------------------------------------------------------
+
+library(tidyverse)
+#library(tidylog)
+library(here)
+
+# 01: GET RAW FILES LIST ----------------------------------------------------------
+
+# get data dir
+simdir <- "sim003"
+#distribution <- "norm"
+coverage <- 100
+
+# get filenames
+filenames <- list.files(path = paste0("results/",simdir), pattern = ".out",full.names = F)
+#head(filenames)
+
+# do some tidying
+files_df <- filenames %>% tibble::enframe(name = "fileno", value = "filename") %>%
+  separate(col = filename, 
+           into = c("run","nInd","nLoci", "theta",
+                    "coverage", "error", "hapThresh",
+                    "distrib", "reps"), 
+           sep = "_", remove = F) %>%
+  select(-fileno, -run, -reps, -hapThresh, -error) %>% 
+  mutate(theta = as.numeric(gsub(x=theta, pattern = "^t", replacement = "")),
+         nInd = as.numeric(nInd),
+         nLoci = as.numeric(nLoci),
+         coverage = as.factor(coverage),
+         distrib = as.factor(distrib),
+         sampleID = glue::glue("{nInd}_{nLoci}_{theta}_{coverage}_{distrib}")) %>% 
+  arrange(nLoci, theta, coverage, nInd, distrib)
+
+#View(files_df)
+
+# save this out
+save(files_df, file = glue::glue("results/{simdir}/{simdir}_files_list_{coverage}x.rda"))
+
+
+# 02: IMPORT FILES  ------------------------------------------
+
+# reads all in at once, keeps data as list col
+library(tictoc) # to time stuff
+library(furrr) # use furrr and work in parallel
+
+# now run in parallel
+tic()
+dataAll <- files_df %>% 
+  #slice(1:100) %>% 
+  mutate(dat = furrr::future_imap(glue::glue("results/{simdir}/{filename}"),
+                                  ~read_tsv(.x, col_names = FALSE),
+                                  .progress = TRUE))
+toc()
+
+# 03: CALCULATE HAPLOTYPES  -------------------------------------
+
+# calculate by LOCI (columnwise, all replicates for one loci)
+f_agg_haps_by_locus <- function(x, stat) {
+  dplyr::summarize_all(x, stat) %>% 
+    pivot_longer(cols=starts_with("X"), names_to="reps")
+}
+
+# calculate by replicate (so rowwise, all loci for one replicate)
+f_agg_haps_by_rep <- function(x, stat) {
+  x %>% unnest(dat) %>% rowwise %>% 
+    group_by(sampleID) %>% summarize_at(vars(starts_with("X")), stat) %>% 
+    pivot_longer(cols=starts_with("X"), names_to="reps")
+}
+
+# Function to make a dataframe and refactor
+f_haps_df <- function(statname, df){
+  df %>% 
+    separate(col = sampleID, into = c("nInd","nLoci", "theta","coverage", "distrib"), sep = "_", remove = F) %>%
+    rename(haplos=value) %>%
+    mutate(stat=statname) %>% 
+    mutate_at(c("nInd","nLoci","theta"), as.numeric) %>%
+    mutate_at(c("distrib", "coverage"), as.factor) %>%  
+    mutate(reps = as.integer(gsub(reps, pattern = "^X",replacement = "")))
+}
+
+
+# ** Calculate Mean -------------------------------------------
+
+# BY LOCI: calculate haplos per loci (over all replicates)
+loc_mean_df <- map(dataAll$dat, ~f_agg_haps_by_locus(.x, "mean")) %>% 
+  map2_df(., files_df$sampleID, ~mutate(.x, sampleID=.y))
+loc_mean_df <- f_haps_df("mean", loc_mean_df) # split out wide
+
+# BY REPLICATE: calculate mean haplos per replicate (over all X loci)
+rep_mean_df <- f_agg_haps_by_rep(dataAll, "mean")
+rep_mean_df <- f_haps_df("mean", rep_mean_df)
+
+
+# ** Calculate Var -------------------------------------------
+
+# BY LOCI: calculate haplos per loci (over all replicates)
+loc_var_df <- map(dataAll$dat, ~f_agg_haps_by_locus(.x, "var")) %>% 
+  map2_df(., files_df$sampleID, ~mutate(.x, sampleID=.y))
+loc_var_df <- f_haps_df("var", loc_var_df)
+
+# BY REPLICATE: calculate haplos per replicate (over all X loci)
+rep_var_df <- f_agg_haps_by_rep(dataAll, "var")
+rep_var_df <- f_haps_df("var", rep_var_df)
+
+
+# ** Calculate Max -------------------------------------------
+
+# BY LOCI: calculate haplos per loci (over all replicates)
+loc_max_df <- map(dataAll$dat, ~f_agg_haps_by_locus(.x, "max")) %>% 
+  map2_df(., files_df$sampleID, ~mutate(.x, sampleID=.y))
+loc_max_df <- f_haps_df("max", loc_max_df)
+
+# BY REPLICATE: calculate haplos per replicate (over all X loci)
+rep_max_df <- f_agg_haps_by_rep(dataAll, "max")
+rep_max_df <- f_haps_df("max", rep_max_df)
+
+# ** Calculate Min -------------------------------------------
+
+# BY LOCI: calculate haplos per loci (over all replicates)
+loc_min_df <- map(dataAll$dat, ~f_agg_haps_by_locus(.x, "min")) %>% 
+  map2_df(., files_df$sampleID, ~mutate(.x, sampleID=.y))
+loc_min_df <- f_haps_df("min", loc_min_df)
+
+# BY REPLICATE: calculate haplos per replicate (over all X loci)
+rep_min_df <- f_agg_haps_by_rep(dataAll, "min")
+rep_min_df <- f_haps_df("min", rep_min_df)
+
+
+# 04: COMBINE INTO SINGLE DATASET ------------------------------
+
+# COMBINE ALL BY LOCUS:
+sim_loc_df <- bind_rows(loc_mean_df, loc_max_df, loc_min_df, loc_var_df) %>% 
+  mutate("Model"=as.factor(glue::glue("{coverage}x {distrib} by Locus")))
+
+# save out
+save(sim_loc_df, file = glue::glue("results/{simdir}/{simdir}_locus_df_{coverage}x.rda"))
+
+# COMBINE ALL BY REPS:
+sim_rep_df <- bind_rows(rep_mean_df, rep_max_df, rep_min_df, rep_var_df) %>% 
+  mutate("Model"=as.factor(glue::glue("{coverage}x {distrib} by Rep")))
+
+# save out
+save(sim_rep_df, file = glue::glue("results/{simdir}/{simdir}_rep_df_{coverage}x.rda"))
+
+
